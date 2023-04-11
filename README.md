a01cf803d2fbdcafd9dd0fbd1a4ae8ba459e5872
Ivan Wu <fgma51f@gmail.com>
2023-04-11 21:04:13 +0800

    Build crawl facebook posts to list func

--- README.md
+++ README.md
@@ -1,13 +1,31 @@
-# crawl-facebook-posts
+# Crawl-facebook-posts
 
-Active selenium grid server
+Crawl facebook groups posts.
 
+## Getting Started
+
+Clone repo from Github and active selenium grid server.
 ```
+git clone https://github.com/51st-F/crawl-facebook-posts.git &&
+cd crawl-facebook-posts &&
 docker-compose -f docker-compose-standalone-chrome.yml up -d
 ```
 
 Active crawler
-
 ```
 docker-compose -f docker-compose-crawler.yml up -d
 ```
+
+
+### Prerequisites
+
+* [Docker-desktop](https://www.docker.com/products/docker-desktop/)
+
+## Authors
+
+* [Ivan Wu](https://github.com/51st-F) - *Initial work* 
+## Acknowledgments
+
+* Those articles I search from Google
+* Those videos I search from Youtube
+* etc
--- src/selenium-remote-standalone-chrome/script_1.py
+++ src/selenium-remote-standalone-chrome/script_1.py
@@ -3,6 +3,7 @@ import pandas as pd
 from selenium import webdriver
 import time
 
+### You may have to check you ip in container names called 'crawl-facebook-posts-chrome_standalone-1' first
 browser = webdriver.Remote(
     command_executor='http://172.27.0.2:4444/wd/hub',
     options=webdriver.ChromeOptions()
@@ -11,48 +12,44 @@ browser = webdriver.Remote(
 url = 'https://www.facebook.com' + '/groups' + '/1260448967306807'
 browser.get(url)
 
-### tag to locate
-article_class_ = "x1yztbdb x1n2onr6 xh8yej3 x1ja2u2z"
-content_class_ = "x11i5rnm xat24cr x1mh8g0r x1vvkbs xdj266r x126k92a"
-more_content_class_ = "x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz xt0b8zv xzsf02u x1s688f"
-text_dir_ = "auto"
-comment_class_ = "x1n2onr6 x4uap5 x18d9i69 x1swvt13 x1iorvi4 x78zum5 x1q0g3np x1a2a7pz"
-more_comment_class_ = "x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x3x7a5m x6prxxf xvq8zen x1s688f xi81zsa"
-
-def check_articles_num(text:str):
+def check_articles_num(text: str):
     """
-    This is an example of a function docstring.
-    More descriptions go here.
+    Use the div class below to sperate articles from raw html.
+    .
     Args:
-        param1 (str): Brower's current page source.
+        text (str): Brower's current page source.
     Returns:
-        int: Brower's current articles number.
-        bool: True if successful, False otherwise.
+        articles_num (int): Brower's current articles number.
+        articles (list): After using beautifulSoup.find_all(), it generate article's full html to list.
     Raises:
-        AttributeError: The ``Raises`` section is a list of all exceptions
-        that are relevant to the interface.
-        ValueError: If `param2` is equal to `param1`.
+        If articles_num equals zero, 
+        you may have to check whether method of locating content is correct or other unexpected problems.
     """
+    articles_div_class = "x1yztbdb x1n2onr6 xh8yej3 x1ja2u2z"
 
     soup = BeautifulSoup(text, "html.parser")
-    articles = soup.find_all("div", class_= article_class_)
+    articles = soup.find_all("div", class_= articles_div_class)
     articles_num = len(articles)
+
+    if articles_num == 0:
+        ValueError('Can not find any facebook articles from your input')
 
     return articles_num, articles
 
-
-time.sleep(3)
-a0, b0 = check_articles_num(browser.page_source)
-browser.execute_script("window.scrollTo(0,document.body.scrollHeight);")
-time.sleep(1)
+time.sleep(2)
+a0, b0 = 0, []
 a1, b1 = check_articles_num(browser.page_source)
+articles_wanted = 50
+
 while True:
-    if a1 > 5:
+    ### Remove the first if-else statement then it'll continuous crawl articles till articles number is not increased after scrolling down.
+    if a1 > articles_wanted:
         break
     else:
         if a0 < a1:
             a0, b0 = a1, b1
             a1, b1 = check_articles_num(browser.page_source)
+            time.sleep(1)
             browser.execute_script("window.scrollTo(0,document.body.scrollHeight);")
         else:
             time.sleep(3)
@@ -66,8 +63,54 @@ while True:
             else:
                 break
 
-# for i in soup.find_all("div", class_= content_class_):
-#     for j in i.find_all("div", dir=text_dir_):
-#         print(j.text)
+def articles_to_list(text: list):
+    """
+    Use the info provided in ('div', {"aria-label":False, "role":"article", "class":"x1a2a7pz"})['aria-describedby'] to locating content and make it to DataFrame.
+
+    Args:
+        text (str): Brower's current page source.
+    Returns:
+        articles_num (int): Brower's current articles number.
+        articles (list): After using beautifulSoup.find_all(), it generate article's full html to list.
+    Raises:
+        If articles_df's row equals zero,
+        you may have to check whether method of locating content is correct or other unexpected problems.
+    """
+    article_list = []
+    for i in text:
+        for j in i.find_all('div', {"aria-label":False, "role":"article", "class":"x1a2a7pz"}):
+            for m in j['aria-describedby'].split(' '):
+                for n in j.find_all("div", {"id":f"{m}", "data-ad-preview":True}):
+                    try:
+                        article_content = n.text
+                    except:
+                        pass
+                for n in j.find_all("div", {"id":f"{m}", "class":"x1iorvi4 x1pi30zi x1l90r2v x1swvt13"}):
+                    try:
+                        article_content = n.text
+                    except:
+                        pass
+                for n in j.find_all('span', {'id':f'{m}'}):
+                    for o in n.find_all('a', {'role':'link'}):
+                        try:
+                            article_link = o['href']
+                            article_link_split = article_link.split('?')[0].split('/')
+                            if article_link_split[-1] == '':
+                                article_id = article_link_split[-2]
+                            else:
+                                article_id = article_link_split[-1]
+                        except:
+                            pass
+        article_list.append([article_id, article_link, article_content])
+
+    articles_df = pd.DataFrame(article_list, columns=['id','link','content'])
+
+    if articles_df.shape[0] == 0:
+        ValueError('Can not find the contents expected from articles')
+    
+    return articles_df
+
+df = articles_to_list(b1)
+df.to_csv('demo.csv',encoding='utf-8', index=False)
 
 browser.quit()
